175ae4eaef3b4cdd028daa87e1592197167d5f13
AlDIY <87589371+dosipod@users.noreply.github.com>
2025-01-29 00:39:45 +0300

    Update usermods_list.cpp

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -250,6 +250,11 @@
   #include "../usermods/usermod_v2_RF433/usermod_v2_RF433.h"
 #endif
 
+#ifdef USERMOD_TTGO_TDISPLAY_OUTPUT
+#include "../usermods/usermod_v2_TTGO-TDisplay_output/usermod.h"
+#endif
+
+
 void registerUsermods()
 {
 /*
@@ -486,4 +491,9 @@ void registerUsermods()
   #ifdef USERMOD_RF433
   UsermodManager::add(new RF433Usermod());
   #endif
+
+  #ifdef USERMOD_TTGO_TDISPLAY_OUTPUT
+  UsermodManager::add(new TTGOTDisplayOutputUsermod());
+  #endif
+  
 }
