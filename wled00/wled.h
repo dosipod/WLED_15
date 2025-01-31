c944feed0ff9b0c60bebf55cad9dbee9d33130be
AlDIY <87589371+dosipod@users.noreply.github.com>
2025-02-01 01:57:38 +0300

    Update wled.h

--- wled00/wled.h
+++ wled00/wled.h
@@ -273,7 +273,7 @@ using PSRAMDynamicJsonDocument = BasicJsonDocument<PSRAM_Allocator>;
 // Global Variable definitions
 WLED_GLOBAL char versionString[] _INIT(TOSTRING(WLED_VERSION));
 WLED_GLOBAL char releaseString[] _INIT(WLED_RELEASE_NAME); // must include the quotes when defining, e.g -D WLED_RELEASE_NAME=\"ESP32_MULTI_USREMODS\"
-#define WLED_CODENAME "Heiwa"
+#define WLED_CODENAME "Oogway"
 
 // AP and OTA default passwords (for maximum security change them!)
 WLED_GLOBAL char apPass[65]  _INIT(WLED_AP_PASS);
